ed062b637b4847a281b78b76df4c13a1d1cfc108
Mariyappan <114395182+MARIYAPPANS@users.noreply.github.com>
2025-01-02 12:18:45 +0530

    Add files via upload

---
+++ DAY-28/INDIVISIBLE.py
@@ -0,0 +1,7 @@
+t = int(input())
+for _ in range(t):
+    a, b, c = map(int, input().split())
+    for k in range(2, 100):
+        if a % k != 0 and b % k != 0 and c % k != 0:
+            print(k)
+            break
